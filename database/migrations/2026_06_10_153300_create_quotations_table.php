0b88f89dae8eabbf3ae4eb73a4555f005e861e3a
khalid-bensaada <kbensaada953@gmail.com>
2026-06-10 16:59:49 +0100

    added tables of this project

---
+++ database/migrations/2026_06_10_153300_create_quotations_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('quotations', function (Blueprint $table) {
+            $table->id();
+
+            
+            $table->foreignId('customer_id')
+                ->constrained('customers')
+                ->onDelete('cascade');
+
+            
+            $table->enum('status', [
+                'pending',
+                'approved',
+                'rejected'
+            ])->default('pending');
+
+            
+            $table->decimal('total', 10, 2)->default(0);
+
+            
+            $table->text('notes')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('quotations');
+    }
+};
---
+++ database/migrations/2026_06_10_153500_create_quotation_items_table.php
@@ -0,0 +1,41 @@
+<?php
+
+
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('quotation_items', function (Blueprint $table) {
+            $table->id();
+
+            
+            $table->foreignId('quotation_id')
+                ->constrained('quotations')
+                ->onDelete('cascade');
+
+            
+            $table->foreignId('product_id')
+                ->constrained('products')
+                ->onDelete('cascade');
+
+            
+            $table->integer('quantity');
+            $table->decimal('price', 10, 2);
+
+            
+            $table->decimal('subtotal', 10, 2)->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('quotation_items');
+    }
+};
